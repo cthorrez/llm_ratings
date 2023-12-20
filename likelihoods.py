ad7f8c0999c5429747b139e4e0f2e814d7a034bb
cthorrez <claytonthorrez@gmail.com>
2023-12-20 18:41:43 -0500

    add rk loglike

--- likelihoods.py
+++ likelihoods.py
@@ -11,4 +11,20 @@ def bt_log_likelihood(ratings, matchups, outcomes, base=math.e, scale=1.0):
     return nll.mean()
 
 def rk_log_likelihood(ratings, matchups, outcomes, theta=1.0, base=math.e, scale=1.0):
-    pass
+    pi = np.exp(ratings)
+    pi_1 = pi[matchups[:,0]]
+    pi_2 = pi[matchups[:,1]]
+    denom_1 = pi_1 + (theta * pi_2)
+    denom_2 = pi_2 + (theta * pi_1)
+    prob_1_win = pi_1 / denom_1
+    prob_2_win = pi_2 / denom_2
+    num = pi_1 * pi_2 * (np.square(theta) - 1.0)
+    prob_draw = num / (denom_1 * denom_2)
+    win_1_mask = outcomes == 1.0
+    win_2_mask = outcomes == 0.0
+    draw_mask = outcomes == 0.5
+    win_1_loglike = np.log(prob_1_win[win_1_mask]).sum()
+    win_2_loglike = np.log(prob_2_win[win_2_mask]).sum()
+    draw_loglike = np.log(prob_draw[draw_mask].sum())
+    loglike = (win_1_loglike + win_2_loglike + draw_loglike) / matchups.shape[0]
+    return loglike
