73dce29967fe75c6699d295db5e825e5939c027c
cthorrez <claytonthorrez@gmail.com>
2024-02-01 23:04:41 -0800

    sweep acc and nll

--- sweep_rk.py
+++ sweep_rk.py
@@ -25,16 +25,28 @@ if __name__ == '__main__':
         }
     }
     seed = 0
-    # metric_keys = ['train_acc', 'test_acc']
-    metric_keys = ['train_nll', 'test_nll']
-    thetas = np.linspace(start=1.01, stop=2.5, num=20)
+    max_metric = True
+    metric_keys = ['train_acc', 'test_acc']
+    # max_metric = False
+    # metric_keys = ['train_nll', 'test_nll']
+    thetas = np.linspace(start=1.0001, stop=2.4, num=1000)
     ys = []
     for theta in tqdm(thetas):
         current_config = deepcopy(rao_kupper_config)
         current_config['args']['theta'] = theta
         metrics = eval_seed(train_df, test_df, [current_config], seed=seed, verbose=False)[0]
-        
         ys.append([metrics[metric_key] for metric_key in metric_keys])
+    ys = np.array(ys)
+
+    # lol that's hacky as hell but I stand by it
+    # (-1)^False = (-1)^0 = 1
+    # (-1)^True = (-1)^1 = -1
+    # thus it switches the sign if max_metric=True so it's an argmax :D
+    train_optimal_idx = np.argmin(ys[:,0] * (-1)**max_metric)
+    train_optimal_theta = thetas[train_optimal_idx]
+    train_optimal_test_metric = ys[:,1][train_optimal_idx]
+    print(f'{train_optimal_theta=}')
+    print(f'{train_optimal_test_metric=}')
     
     plt.plot(thetas, ys)
     plt.xlabel('theta')
